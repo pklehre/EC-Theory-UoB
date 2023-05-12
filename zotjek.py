085711fdd63b08d667916386820d0819b499cf73
asbenford <124168821+asbenford@users.noreply.github.com>
2023-05-12 12:22:04 +0000

    Deploying to gh-pages from @ mariohevia/EC-Theory-UoB@56cb8c049055d8d57b7c6f15185072d5ec235556 🚀

---
+++ zotjek.py
@@ -0,0 +1,9 @@
+from pyzotero import zotero
+import bibtexparser
+
+zot = zotero.Zotero(library_id='4980316',library_type='group',api_key='Z6twDFkFD0lMq4gNPFv7bMNu')
+zot.add_parameters(format='bibtex',style='abbrv')   # Change style as desired.
+bib_db = zot.top()
+
+with open('references.bib','w') as bibtex_file:
+    bibtexparser.dump(bib_db, bibtex_file)
