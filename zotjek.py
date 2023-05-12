22e0fb58395ed0a8faa9b2dcea76bddd7c8dc389
Mario Hevia <mario_hevia@hotmail.com>
2023-05-12 14:07:00 +0100

    Added references

--- zotjek.py
+++ zotjek.py
@@ -5,5 +5,5 @@ zot = zotero.Zotero(library_id='4980316',library_type='group',api_key='Z6twDFkFD
 zot.add_parameters(format='bibtex',style='abbrv')   # Change style as desired.
 bib_db = zot.top()
 
-with open('references.bib','w') as bibtex_file:
+with open('_bibliography/references.bib','w') as bibtex_file:
     bibtexparser.dump(bib_db, bibtex_file)
