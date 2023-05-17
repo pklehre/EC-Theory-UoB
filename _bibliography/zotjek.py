945199a1e6f55ee991eebb39e2ba6b871fdbca1c
Mario Hevia <mario_hevia@hotmail.com>
2023-05-17 14:49:32 +0100

    Added parsing for bib entries with video,pdf,etc from zotero

---
+++ _bibliography/zotjek.py
@@ -0,0 +1,29 @@
+from pyzotero import zotero
+import bibtexparser
+from bibtexparser.bwriter import BibTexWriter
+
+zot = zotero.Zotero(library_id='4980316',library_type='group',api_key='Z6twDFkFD0lMq4gNPFv7bMNu')
+zot_db = zot.everything(zot.top(sort='date'))
+writer = BibTexWriter()
+writer.add_trailing_comma = True
+str_bibs = ''
+
+for bib in zot_db:
+    extra = bib['data']['extra']
+    itemID = bib['key']
+    bib_item = zot.item(itemID,format='bibtex',style='abbrv')
+    bib_item.entries[0]['note']=''
+    str_bib = bibtexparser.dumps(bib_item, writer).split('\n')
+    if len(extra)!=0:
+        str_bib.insert(-2, " bibtex_show = {true},"+"\n "+extra)
+    else:
+        str_bib.insert(-2, " bibtex_show = {true},")
+    str_bibs += '\n'.join(str_bib)+'\n'
+
+with open('_bibliography/references.bib','w') as bibtex_file:
+    bibtex_file.write(str_bibs)
+
+# with open('_bibliography/references.bib','w') as bibtex_file:
+#     bibtexparser.dump(bib_db, bibtex_file, writer)
+
+
--- zotjek.py
+++
@@ -1,9 +0,0 @@
-from pyzotero import zotero
-import bibtexparser
-
-zot = zotero.Zotero(library_id='4980316',library_type='group',api_key='Z6twDFkFD0lMq4gNPFv7bMNu')
-zot.add_parameters(format='bibtex',style='abbrv')   # Change style as desired.
-bib_db = zot.top()
-
-with open('_bibliography/references.bib','w') as bibtex_file:
-    bibtexparser.dump(bib_db, bibtex_file)
